0a474fa3b33b352053fe4042c13e15a3baced69f
Kamrul Hasan <113679107+Kamrul-Hasan-2@users.noreply.github.com>
2023-10-01 23:09:52 +0600

    Quick_sort

--- Mege_sort.cpp
+++ Mege_sort.cpp
@@ -53,5 +53,4 @@ int main(){
      for(int i=0; i<n; i++){
         cout << A[i] << " ";
     }
-
 }
---
+++ Quick_sort.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int part_index(int* A, int start, int ends){
+    int pivot = A[ends];
+    int PIdx = start;
+    for(int i= start; i<ends; i++){
+        if(A[i] <= pivot){
+            swap(A[i], A[PIdx]);
+            PIdx++;
+        }
+    }
+    swap(A[PIdx], A[ends]);
+    return PIdx;
+}
+
+void Quick_sort(int* A, int start , int ends){
+    if(start<ends){
+        int idx = part_index(A, start, ends);
+        Quick_sort(A, start, idx-1);
+        Quick_sort(A, idx+1, ends);
+    }
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    int A[n];
+
+    for(int i=0; i<n; i++){
+        cin >> A[i];
+    }
+    Quick_sort(A, 0, n-1);
+     for(int i=0; i<n; i++){
+        cout << A[i] << " ";
+    }
+}
